367fc115ee9d73dc2291b3590da032addd1b2c4c
Lee <delutejohnz@gmail.com>
2022-05-23 03:41:56 +0800

    apdit

--- src/components/first.js
+++ src/components/first.js
@@ -15,9 +15,9 @@ function First() {
                         </button>   
                     </div>
                 </div>
-                <div className='text-center xs:py-[30px] md:py-[140px] space-y-5 '>
-                        <h1 className='xs:leading-[3rem] w-[50%] xs:text-[40px] md:text-[58px] font-extrabold md:tracking-wide fade-in-no-delay mx-auto'>Build A Brand</h1>
-                        <p className='xs:leading-[1.40rem] md:leading-[1.75rem] font-[500] text-[18px] w-[60%] xs:tracking-tighter md:tracking-wide mx-auto fade-in-delay'>
+                <div className='xs:grid xs:place-items-center xs:place-content-center xs:h-[100%] md:relative md:bottom-[55px] text-center space-y-5'>
+                        <h1 className='xs:leading-[3rem] w-[50%] xs:text-[40px] md:text-[58px] font-extrabold md:tracking-wide fade-in-no-delay'>Build A Brand</h1>
+                        <p className='xs:leading-[1.40rem] md:leading-[1.75rem] font-[500] text-[18px] w-[60%] xs:tracking-tighter md:tracking-wide fade-in-delay'>
                         Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sed iusto debitis consequuntur molestias deleniti, vel est earum pariatur vero cupiditate, aut eaque temporibus quis atque. Obcaecati labore sit ab! Et?
                         </p>
                 </div>
